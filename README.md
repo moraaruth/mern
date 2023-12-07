a6ac3fd74e2f88fad3839bdeef03d4e2ca3854ea
Ruth Moraa <moraar37@gmail.com>
2023-12-07 10:33:12 +0300

    installing dependencies and setting up environment and sample data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mern
 mern
+## npm init -y 
+## npm install express express-graphql graphql mongoose cors colors
+## npm i -D nodemon dotenv
+
+# Graphql
+## const { graphQlHTTP } = require('express-graphql')
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('/schema/schema')
+const port = process.env.PORT || 5000
+
+
+
+const app = express();
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: process.env.NODE_ENV === 'development',
+    })
+  )
+
+app.listen(port, console.log(`Server running on port ${port}`));
